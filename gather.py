fbfd463b307ee47c2fc79a5ce8d0893a68595a8a
GitBolt <stingbolt1@gmail.com>
2021-10-07 00:06:13 +0530

    🔧 Better functions and optional file overriding

--- gather.py
+++ gather.py
@@ -25,18 +25,24 @@ def gather_context(messages=gather_messages(), **kwargs):
         msg_id = message["id"]
         content = message["content"]
         context_url = f"https://discord.com/api/v9/channels/870330763772563481/messages?limit=3&around={msg_id}"
+
         resp = requests.get(context_url, headers=HEADERS).json()
         sorted_resp = sorted(resp, key=lambda i: i["id"])
         context_content = sorted_resp[0]["content"]
-        conversations.append({"context": context_content, "reply": content})
-        print(f"Added {idx+1} conversation group")
+        conversations.append({"tag": idx, "context": context_content, "reply": content})
+        print(f"Loaded {idx+1} conversation group")
         time.sleep(1) # Preventing rate limit
 
-    print(conversations)
     if kwargs.get("save"):
+        if os.path.isfile("conversations.txt"):
+            inp = input("Found existing conversations.txt file, do you wish to override it? y\\n: ")
+            if inp != "y":
+                return
         file = open("conversations.txt", "w+")
         file.write(json.dumps(conversations))
         file.close()
-        print("\nSaved gathered messages in messages.txt")
+        print("Saved gathered messages in messages.txt file")
+    else:
+        print("-"*100, f"\n{conversations}\n", "-"*100)
 
-gather_context(save=True, limit=2)
+gather_context(limit=2)
